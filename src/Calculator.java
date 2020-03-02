a917d5f59000edbf0d2eb779d08ff10d44634503
yoatn <yoatn@bk.ru>
2020-03-02 10:43:09 +0300

    add road map

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+/*
+План:
+1. Ввод чисел и знака
+2. Ввод из одной строки
+3. Ввод римских цифр
+4. Постоянный ввод
+5. Обработка исключений:
+    a. Неверный шаблон ввода (Количество, пробелы, символы) ...may be RegEx?
+    b. Пустой ввод
+    c.
+    d.
+    e.
+    g.
+6.
+ */
 
-public class calculator {
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner reader = new Scanner(System.in);
+
+            System.out.println("Введите число 1: ");
+            int digitOne = reader.nextInt();
+//            System.out.println(digitOne);
+
+            System.out.println("Введите число 2: ");
+            int digitTwo = reader.nextInt();
+//            System.out.println(digitTwo);
+
+            System.out.println("Введите знак: ");
+            char operand = reader.next().charAt(0);
+//            System.out.println(operand);
+        }
+    }
 }
+
