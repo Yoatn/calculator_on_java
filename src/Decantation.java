cfc2e1f90c75d92234d9d97260e3c1aab7afbe75
yoatn <you@example.com>
2020-03-09 03:18:25 +0300

    added the initial greeting to the calculator. Added RegEx expression validation in the Decantation

--- src/Decantation.java
+++ src/Decantation.java
@@ -8,15 +8,29 @@ public class Decantation {
             System.out.println("Введите выражение: ");
             Scanner scanner = new Scanner(System.in);
             String myString = scanner.nextLine();
-            String[] myCharArray = myString.split(" ");
-
-            int operand_1 = Integer.parseInt(myCharArray[0]);
-            int operand_2 = Integer.parseInt(myCharArray[2]);
-            char operator = myCharArray[1].charAt(0);
-
-            System.out.println(operand_1);
-            System.out.println(operand_2);
-            System.out.println(operator);
+            if (myString.matches("^([0-9]|10)\\s[\\-|\\+|\\*|\\/]\\s([0-9]|10)$|" +
+                    "^(I|II|III|IV|V|VI|VII|VIII|IX|X)\\s[\\-|\\+|\\*|\\/]\\s(I|II|III|IV|V|VI|VII|VIII|IX|X)$")) {
+                System.out.println(myString);
+                System.out.println();
+            } else {
+                System.out.println("Неверное выражение. Повторите ввод.");
+                System.out.println();
+            }
+//            String[] myCharArray = myString.split(" ");
+//
+//            int operand_1 = Integer.parseInt(myCharArray[0]);
+//            int operand_2 = Integer.parseInt(myCharArray[2]);
+//            char operator = myCharArray[1].charAt(0);
+//
+//            System.out.println(operand_1);
+//            System.out.println(operand_2);
+//            System.out.println(operator);
         }
     }
 }
+
+
+// ^\d{1,2}\s[\-|\+|\*|\/]\s\d{1,2} arabic
+// ^(I|II|III|IV|V|VI|VII|VIII|IX|X)\s[\-|\+|\*|\/]\s(I|II|III|IV|V|VI|VII|VIII|IX|X)$ rome
+
+// ^([0-9]|10)\s[\-|\+|\*|\/]\s([0-9]|10)$|^(I|II|III|IV|V|VI|VII|VIII|IX|X)\s[\-|\+|\*|\/]\s(I|II|III|IV|V|VI|VII|VIII|IX|X)$
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Приветствую!\n" +
+                "Я Калькулятор.\n" +
+                "Я умею складывать, вычитать, делить и умножать числа от 0 до 10.\n" +
+                "Числа могут быть, как арабскими, так римскими.\n" +
+                "Римские цифры должны иметь вид типа I, VIII, IX и т.д.\n" +
+                "Выражение вводится строго по шаблону (без {}):\n" +
+                "{число}{пробел}{знак +-*/}{пробел}{число}.\n" +
+                "Например: 2 + 4 или III + IX");
         while (true) {
-            System.out.println("Введите выражение: ");
+            System.out.println("Введите выражение:");
             Scanner scanner = new Scanner(System.in);
             String myString = scanner.nextLine();
 
@@ -19,7 +27,7 @@ public class Main {
 
             // Вывод результата
             System.out.println("________");
-            System.out.println(result);
+            System.out.println("Ваш ответ : " + result);
             System.out.println();
         }
     }
